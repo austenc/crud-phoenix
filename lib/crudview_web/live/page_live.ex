bf3aa42053691d9a971fbfddeef6fbae61816922
Austen Cameron <austen.cameron@gmail.com>
2020-12-11 12:42:27 -0700

    paginated a table

--- lib/crudview_web/live/page_live.ex
+++ lib/crudview_web/live/page_live.ex
@@ -4,29 +4,21 @@ defmodule CrudviewWeb.PageLive do
   alias Crudview.Repo
 
   def mount(_params, _session, socket) do
-    # trades = if connected?(socket), do: Trade.list_trades, else: []
-    trades = Repo.paginate(Trade)
+    {:ok, assign(socket, get_trades())}
+  end
 
-    assigns = [
+  def get_trades(page \\ 1) do
+    trades = Repo.paginate(Trade, page: page)
+    [
       trades: trades.entries,
       page_number: trades.page_number,
       page_size: trades.page_size,
       total_pages: trades.total_pages,
       total_entries: trades.total_entries,
     ]
-    {:ok, assign(socket, assigns)}
   end
 
   def handle_event("nav", %{"page" => page}, socket) do
-     trades = Repo.paginate(Trade, page: page)
-
-    assigns = [
-      trades: trades.entries,
-      page_number: trades.page_number,
-      page_size: trades.page_size,
-      total_pages: trades.total_pages,
-      total_entries: trades.total_entries,
-    ]
-    {:noreply, assign(socket, assigns)}
+    {:noreply, assign(socket, get_trades(page))}
   end
 end
